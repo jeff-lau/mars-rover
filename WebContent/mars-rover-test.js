dc09cd2105d67e5b62edb5a6810533271c34932f
jeff-lau <Jeff.ch.lau@gmail.com>
2013-11-20 22:43:53 +1100

    Tests and code working.  Just need to hook out to input and display output

--- WebContent/mars-rover-test.js
+++ WebContent/mars-rover-test.js
@@ -1,9 +1,23 @@
 //YUI Library used for unit testing.
 YUI().use('test', 'yui-testresult-parser', 'test-console', function(Y){
 	
+	var getStartCoordHelper = function(X, Y, bearing){
+		return {
+			gridX : 5,
+			gridY : 5,
+			startCoord : {
+				X : X,
+				Y : Y,
+				bearing : bearing
+			}
+		}
+	};
+	
+	
 	var roverTestCase = new Y.Test.Case({
 		name : 'Rover Test Case',
 		
+	
 		testInputParser : function(){
 			var testInput = "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM";
 			
@@ -25,14 +39,134 @@ YUI().use('test', 'yui-testresult-parser', 'test-console', function(Y){
 			Y.Assert.areEqual(parsedInput.rovers[0].commands.length, 9, "9 command characters were expected for rover 1");
 			Y.Assert.areEqual(parsedInput.rovers[1].commands.length, 10, "10 command characters were expected for rover 2");
 		},
+	
+		testExecuteCommandCharM : function(){
+			
+			// Move Up
+			var startCoords = getStartCoordHelper(0, 0, 'N');
+			var startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 0, "Expected X Coordinate to stay at 0");
+			Y.Assert.areEqual(startCoord.Y, 1, "Expected Y Coordinate to go from 0 to 1");
+
+			// Move down
+			startCoords = getStartCoordHelper(0, 1, 'S');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 0, "Expected X Coordinate to stay at 0");
+			Y.Assert.areEqual(startCoord.Y, 0, "Expected Y Coordinate to go from 1 to 0");
+
+			// Move Left
+			startCoords = getStartCoordHelper(1, 0, 'W');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 0, "Expected X Coordinate to go from 1 to 0");
+			Y.Assert.areEqual(startCoord.Y, 0, "Expected Y Coordinate to stay at 0");
+
+			// Move Right
+			startCoords = getStartCoordHelper(0, 0, 'E');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 1, "Expected X Coordinate to go from 0 to 1");
+			Y.Assert.areEqual(startCoord.Y, 0, "Expected Y Coordinate to stay at 0");
+
+			//Boundary cases:
+			startCoords = getStartCoordHelper(0, 0, 'W');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 0, "Expected X Coordinate to stay at 0");
+			Y.Assert.areEqual(startCoord.Y, 0, "Expected Y Coordinate to stay at 0");
+			
+			startCoords = getStartCoordHelper(0, 0, 'S');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 0, "Expected X Coordinate to stay at 0");
+			Y.Assert.areEqual(startCoord.Y, 0, "Expected Y Coordinate to stay at 0");
+
+			startCoords = getStartCoordHelper(5, 5, 'N');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 5, "Expected X Coordinate to stay at 5");
+			Y.Assert.areEqual(startCoord.Y, 5, "Expected Y Coordinate to stay at 5");
+
+			startCoords = getStartCoordHelper(5, 5, 'E');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'M', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.X, 5, "Expected X Coordinate to stay at 5");
+			Y.Assert.areEqual(startCoord.Y, 5, "Expected Y Coordinate to stay at 5");
+
+		},
+
+
+		testExecuteCommandCharL : function(){
+			
+			var startCoords = getStartCoordHelper(0, 0, 'N');
+			var startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'L', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 'w', "Expected Bearing to go from N to W");
+			
+			startCoords = getStartCoordHelper(0, 0, 'W');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'L', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 's', "Expected Bearing to go from W to S");
+
+			startCoords = getStartCoordHelper(0, 0, 'S');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'L', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 'e', "Expected Bearing to go from S to E");
+
+			startCoords = getStartCoordHelper(0, 0, 'E');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'L', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 'n', "Expected Bearing to go from E to N");
+
+		},
+		
+		
+		testExecuteCommandCharR : function(){
+			var startCoords = getStartCoordHelper(0, 0, 'N');
+			var startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'R', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 'e', "Expected Bearing to go from N to E");
+			
+			startCoords = getStartCoordHelper(0, 0, 'E');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'R', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 's', "Expected Bearing to go from E to S");
+
+			startCoords = getStartCoordHelper(0, 0, 'S');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'R', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 'w', "Expected Bearing to go from S to W");
+
+			startCoords = getStartCoordHelper(0, 0, 'W');
+			startCoord = startCoords.startCoord;
+			roverModule.executeCommandChar(startCoords.startCoord, 'R', startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoord.bearing, 'n', "Expected Bearing to go from W to N");
+		},
+		
 		
 		testCalculatePosition : function(){
-			roverModule.calculatePosition();
 			
-			Y.Assert.areEqual(5, 6, "Five was expected.");
+			var startCoords = getStartCoordHelper(1, 2, 'N');
+			var commands = ['L','M','L','M','L','M','L','M','M'];
+			
+			roverModule.calculatePosition(startCoords.startCoord, commands, startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(1, startCoords.startCoord.X, "X coordinate of 3 was expected");
+			Y.Assert.areEqual(3, startCoords.startCoord.Y, "Y coordinate of 3 was expected");
+			Y.Assert.areEqual(startCoords.startCoord.bearing, 'n', "Bearing of N was expected");
+			
+			startCoords = getStartCoordHelper(3, 3, 'E');
+			commands = ['M','M','R','M','M','R','M','R','R','M'];
+			roverModule.calculatePosition(startCoords.startCoord, commands, startCoords.gridX, startCoords.gridY);
+			Y.Assert.areEqual(startCoords.startCoord.X, 5, "X coordinate of 3 was expected");
+			Y.Assert.areEqual(startCoords.startCoord.Y, 1, "Y coordinate of 3 was expected");
+			Y.Assert.areEqual(startCoords.startCoord.bearing, 'e', "Bearing of E was expected");
 		}
 		
 	});
+	
+	
 	
 	
 	  //initialize the console
--- WebContent/mars-rover.js
+++ WebContent/mars-rover.js
@@ -3,9 +3,86 @@ var roverModule = {};
 	
 	
 	
-	var calculatePosition = function (start_coord, command){
-		//alert('I m in calculate position!!')
-	}
+	var calculatePosition = function (start_coord, commands, gridX, gridY){
+		debugger;
+		for (var i = 0; i < commands.length; i++){
+			executeCommandChar(start_coord, commands[i], gridX, gridY);
+		}
+	};
+	
+	
+	/**
+	 * Executes a single character comand.
+	 */
+	var executeCommandChar = function(start_coord, command, gridX, gridY){
+		var commandLowerCase = command.toLowerCase();
+		switch (commandLowerCase){
+			case 'm' :
+				executeCommandCharM(start_coord, gridX, gridY);
+				break;
+			case 'l':
+				executeCommandCharLR(start_coord, 'l');
+				break;
+			case 'r':
+				executeCommandCharLR(start_coord, 'r');
+				break;
+			default:
+				alert('Unknown command received - ' + command);
+		}
+	};
+	
+	
+	/**
+	 * Executes the M command based on current bearing.
+	 * Will performs the bounds check to ensure rover doesn't move off chart.
+	 * 
+	 */
+	var executeCommandCharM = function(start_coord, gridX, gridY){
+		var currentBearing = start_coord.bearing.toLowerCase();
+		switch (currentBearing){
+			case 'n' :
+				if (start_coord.Y < gridY){
+					start_coord.Y++;
+				}
+				break;
+			case 'e':
+				if (start_coord.X < gridX){
+					start_coord.X++;
+				}
+				break;
+			case 's':
+				if (start_coord.Y > 0){
+					start_coord.Y--;
+				}
+				break;
+			case 'w':
+				if (start_coord.X > 0){
+					start_coord.X--;
+				}
+				break;
+			default:
+				alert('Unknown bearng - ' + commandLowerCase);
+		}
+	};
+	
+	/**
+	 * Updates 
+	 * 
+	 */
+	var executeCommandCharLR = function(start_coord, command){
+		var commandLowercase = command.toLowerCase();
+		var currentBearning = start_coord.bearing.toLowerCase();
+		var directions = ['n', 'e', 's', 'w'];
+		if (commandLowercase === 'l'){
+			directions.reverse();
+		}
+		var index = directions.indexOf(currentBearning) + 1;
+		if (index > 3){
+			index = 0;
+		}
+		start_coord.bearing = directions[index];
+	};
+	
 
 	/**
 	 * Parses the input commands.
@@ -33,15 +110,13 @@ var roverModule = {};
 			gridX : gridX,
 			gridY : gridY,
 			rovers : rovers
-		}
-	}
+		};
+	};
 	
 	
 	roverModule.parseInput = parseInput;
 	roverModule.calculatePosition = calculatePosition;
-	
-	
-	
+	roverModule.executeCommandChar = executeCommandChar;
 	
 	
 }(roverModule));
